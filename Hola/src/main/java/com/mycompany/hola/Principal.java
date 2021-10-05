8a4dec255a4ead5edc604d3286d3e9582971f7e5
Mauricio <mauricio.jerez@hotmail.cl>
2021-10-05 12:54:20 -0300

    cambios prueba

--- Hola/src/main/java/com/mycompany/hola/Principal.java
+++ Hola/src/main/java/com/mycompany/hola/Principal.java
@@ -1,6 +1,6 @@
 package com.mycompany.hola;
 public class Principal {
     public static void main(String[] args) {
-    	 System.out.println("hola mundo	");
+    	 System.out.println("hola mundosss");
     }
 }
